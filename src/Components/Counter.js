e05bd1b11ffaf3c0f962ac52eca5cda169f428d2
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-11-14 11:22:22 +0100

    good counter

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  const [count, setCount] = useState(null);
+  
   // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount] = useState(0);
+  const handleClick = (e) => {
+    setCount(parseFloat(e.target.value))
+  }
+  
 
   return (
     <div>
       <p>Le compteur est  à :</p>
-      <input type = 'number' name = 'counter' value= {count}></input>
+      <input 
+      type = 'number' 
+      name = 'counter' 
+      value= {count}
+      onChange={handleClick}
+      ></input>
       <button onClick={() => setCount(count + 1)}>
         +1
       </button>
